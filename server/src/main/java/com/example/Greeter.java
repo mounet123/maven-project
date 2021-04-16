90d1aa36a1e76feb6bf3d393fc1c046b241c14fd
mounet123 <59195995+mounet123@users.noreply.github.com>
2021-04-16 14:54:21 +0200

    Update Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -7,7 +7,11 @@ public class Greeter {
   public Greeter() {
 
   }
-
+/**
+*@return greeting string
+*@param someone name of a person
+*
+*/
   public final String greet(final String someone) {
     return String.format("Hello babacar, %s!", someone);
   }
